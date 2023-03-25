b071f36d0f51d010b085457578ad3ec5c878d4bf
amandekate <56401874+amandekate@users.noreply.github.com>
2023-03-25 23:53:19 +0530

    Added New repo origin

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+      <LinkColumn>
+      <LinkTitle>Call</LinkTitle>
+      <LinkItem href="tel:7987572664">7987572664</LinkItem>
+      </LinkColumn>
+      <LinkColumn>
+      <LinkTitle>Email</LinkTitle>
+      <LinkItem href="mailto:amandekate007@gmail.com">
+        amandekate007@gmail.com
+        </LinkItem>
+      </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+        <SocialIcons href="https://github.com">
+        <AiFillGithub size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://linledin.com">
+        <AiFillLinkedin size="3rem"/>
+        </SocialIcons><SocialIcons href="https://instagram.com">
+        <AiFillInstagram size="3rem"/>
+        </SocialIcons>
+          </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
